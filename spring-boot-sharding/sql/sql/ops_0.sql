931d7a66db03416451762de780d2247cadc869bb
xurq <xurq@myzh.com>
2019-10-29 17:46:16 +0800

    增加过滤

---
+++ spring-boot-sharding/sql/sql/ops_0.sql
@@ -0,0 +1,232 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : ops_0
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 29/10/2019 17:43:23
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_company
+-- ----------------------------
+DROP TABLE IF EXISTS `t_company`;
+CREATE TABLE `t_company`  (
+  `company_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`company_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_0`;
+CREATE TABLE `t_order_2019_0`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_1`;
+CREATE TABLE `t_order_2019_1`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_2`;
+CREATE TABLE `t_order_2019_2`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_3`;
+CREATE TABLE `t_order_2019_3`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_0`;
+CREATE TABLE `t_order_2020_0`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_1`;
+CREATE TABLE `t_order_2020_1`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_2`;
+CREATE TABLE `t_order_2020_2`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_3`;
+CREATE TABLE `t_order_2020_3`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_province
+-- ----------------------------
+DROP TABLE IF EXISTS `t_province`;
+CREATE TABLE `t_province`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prov_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `prov_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `prov_type` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '1 - 直辖市\r\n2 - 行政省\r\n3 - 自治区\r\n4 - 特别行政区\r\n5 - 其他国家\r\n见全局数据字典[省份类型] \r\n',
+  `prov_state` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '0 - 禁用\r\n1 - 启用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_0`;
+CREATE TABLE `t_user_info_0`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_1`;
+CREATE TABLE `t_user_info_1`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 395510036840841217 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_info_1
+-- ----------------------------
+INSERT INTO `t_user_info_1` VALUES (394888608823115776, 0, 'Name.5', 'Account.5', 'CNiBtrnY', 1572078380843);
+INSERT INTO `t_user_info_1` VALUES (395510036840841216, 0, 'Name.5', 'Account.5', 'CnBhnLeY', 1572226541000);
+
+-- ----------------------------
+-- Table structure for t_user_info_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_2`;
+CREATE TABLE `t_user_info_2`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 395509531339128833 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_info_2
+-- ----------------------------
+INSERT INTO `t_user_info_2` VALUES (394889963830771712, 0, 'Name.5', 'Account.5', 'AqXOkecn', 1572078702056);
+INSERT INTO `t_user_info_2` VALUES (394891629619576832, 0, 'Name.5', 'Account.5', 'tazdPNPr', 1572079100886);
+INSERT INTO `t_user_info_2` VALUES (395509531339128832, 0, 'Name.5', 'Account.5', 'qKFhzIQW', 1572226420360);
+
+-- ----------------------------
+-- Table structure for t_user_info_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_3`;
+CREATE TABLE `t_user_info_3`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 395510439548551169 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_info_3
+-- ----------------------------
+INSERT INTO `t_user_info_3` VALUES (394890102238609408, 0, 'Name.5', 'Account.5', 'BzLqAcwV', 1572078737075);
+INSERT INTO `t_user_info_3` VALUES (394891983174238208, 0, 'Name.5', 'Account.5', 'JAqsWFnD', 1572079185281);
+INSERT INTO `t_user_info_3` VALUES (395510413103464448, 0, 'Name.5', 'Account.5', 'GXKlactR', 1572226630711);
+INSERT INTO `t_user_info_3` VALUES (395510439548551168, 0, 'Name.5', 'Account.5', 'UTIkTzfR', 1572226637015);
+
+-- ----------------------------
+-- Table structure for t_user_info_4
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_4`;
+CREATE TABLE `t_user_info_4`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 395510495643172865 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_info_4
+-- ----------------------------
+INSERT INTO `t_user_info_4` VALUES (394891101946773504, 0, 'Name.5', 'Account.5', 'KfdnvWRv', 1572078975425);
+INSERT INTO `t_user_info_4` VALUES (394892625317986304, 0, 'Name.5', 'Account.5', 'UrCTXArf', 1572079338623);
+INSERT INTO `t_user_info_4` VALUES (395510495643172864, 0, 'Name.5', 'Account.5', 'umVZPWjI', 1572226650390);
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ spring-boot-sharding/sql/sql/ops_2.sql
@@ -0,0 +1,204 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : ops_2
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 29/10/2019 17:44:24
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_company
+-- ----------------------------
+DROP TABLE IF EXISTS `t_company`;
+CREATE TABLE `t_company`  (
+  `company_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`company_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_0`;
+CREATE TABLE `t_order_2019_0`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_1`;
+CREATE TABLE `t_order_2019_1`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_2`;
+CREATE TABLE `t_order_2019_2`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2019_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2019_3`;
+CREATE TABLE `t_order_2019_3`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_0`;
+CREATE TABLE `t_order_2020_0`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_1`;
+CREATE TABLE `t_order_2020_1`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_2`;
+CREATE TABLE `t_order_2020_2`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_order_2020_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2020_3`;
+CREATE TABLE `t_order_2020_3`  (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_province
+-- ----------------------------
+DROP TABLE IF EXISTS `t_province`;
+CREATE TABLE `t_province`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prov_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `prov_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `prov_type` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '1 - 直辖市\r\n2 - 行政省\r\n3 - 自治区\r\n4 - 特别行政区\r\n5 - 其他国家\r\n见全局数据字典[省份类型] \r\n',
+  `prov_state` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '0 - 禁用\r\n1 - 启用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_0`;
+CREATE TABLE `t_user_info_0`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_1`;
+CREATE TABLE `t_user_info_1`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_2
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_2`;
+CREATE TABLE `t_user_info_2`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_3
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_3`;
+CREATE TABLE `t_user_info_3`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user_info_4
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_info_4`;
+CREATE TABLE `t_user_info_4`  (
+  `user_id` bigint(19) NOT NULL AUTO_INCREMENT,
+  `province_id` bigint(19) NULL DEFAULT NULL,
+  `user_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uid` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
